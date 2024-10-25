c6d96059bc6e1556e73826efb463bc312525086a
matryseguros <guimaraes.uenf@gmail.com>
2024-10-25 11:40:39 -0300

    Update index.js

--- java/index.js
+++ java/index.js
@@ -1,3 +1,7 @@
 function btnClicked(){
     setTimeout(function () { window.location.href="https://villa.segfy.com/Publico/Segurados/Orcamentos/SolicitarCotacao?e=KL2BucbW1Ptm5UTtDwv%2BQuahPSYUaG2U"; }, 1100);
 }
+
+function btnEmailClicked(){
+    { window.location.href="mailto:carmem@matryseguros.com.br" };
+}
