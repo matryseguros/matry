efa191ea323349cad7e145effd4966798802693e
matryseguros <guimaraes.uenf@gmail.com>
2024-10-27 18:58:33 -0300

    Update index.js

--- java/index.js
+++ java/index.js
@@ -2,6 +2,15 @@ function btnClicked(){
     setTimeout(function () { window.location.href="https://villa.segfy.com/Publico/Segurados/Orcamentos/SolicitarCotacao?e=KL2BucbW1Ptm5UTtDwv%2BQuahPSYUaG2U"; }, 1100);
 }
 
-function btnEmailClicked(){
+<!-- function btnEmailClicked(){
     { window.location.href="https://matryseguros.github.io/matry" };
+} -->
+var ua = navigator.userAgent || window.opera;
+var isInstagram = (ua.indexOf('Instagram') > -1) ? true : false;
+
+if (document.documentElement.classList ){
+	if (isInstagram) {
+		window.document.body.classList.add('instagram-browser');
+    alert("debugging within the Instagram in-app browser");
+	}
 }
